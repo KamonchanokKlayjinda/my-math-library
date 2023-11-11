0d57616d2e9a72fedea7ec194f5748648c6ef086
Kamonchanok Klayjinda <kamonchanok.klayjinda@gmail.com>
2023-11-11 10:50:37 +0700

    Refactor the sum method
    use Array.foreach

--- main.test.js
+++ main.test.js
@@ -1,5 +1,5 @@
 const math = require('./main')
 const assert = require('assert')
 
-assert.strictEqual(math.sum(3,4),4, 'should sum two numbers')
+assert.strictEqual(math.sum(1,2),3, 'should sum two numbers')
 assert.strictEqual(math.sum([1,2,3,4,5]), 15, 'should sum two numbers')
